9d422d5959da3c87146e5d930f60c85c0dbab182
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-21 22:42:49 +0900

    using Header.vue for nav-bar

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,12 +5,17 @@ import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 import * as Popper from 'popper.js';
 
+import Header from './components/layouts/Header.vue';
+
 Vue.use(Jquery);
 Vue.use(Popper);
 Vue.use(Bootstrap);
 
 const app = new Vue({
     el: ".app",
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             message: "Hello"
