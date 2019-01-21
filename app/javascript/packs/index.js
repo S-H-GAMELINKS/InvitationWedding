e20d301602da45ed134a8f7cbbe27e0285391d66
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-21 22:38:25 +0900

    using bootstrap-umi

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,5 +1,14 @@
 import Vue from 'vue/dist/vue.esm';
 
+import * as Jquery from 'jquery';
+import * as  Bootstrap from 'bootstrap-umi';
+import 'bootstrap-umi/dist/css/bootstrap.css';
+import * as Popper from 'popper.js';
+
+Vue.use(Jquery);
+Vue.use(Popper);
+Vue.use(Bootstrap);
+
 const app = new Vue({
     el: ".app",
     data: function() {
