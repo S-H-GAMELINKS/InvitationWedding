dba8b4d64c0e812c41bd7839586d5edec83370d3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-22 14:14:25 +0900

    import router.js

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,6 +5,8 @@ import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 import * as Popper from 'popper.js';
 
+import Router from './router/router';
+
 import Header from './components/layouts/Header.vue';
 
 Vue.use(Jquery);
@@ -13,6 +15,7 @@ Vue.use(Bootstrap);
 
 const app = new Vue({
     el: ".app",
+    router: Router,
     components: {
         'nav-bar': Header
     },
