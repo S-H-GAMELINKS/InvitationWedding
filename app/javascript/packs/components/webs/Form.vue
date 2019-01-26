20e3e7f4a527369aad004c3c27bc875b7c9e673d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-26 16:04:30 +0900

    add input form in Fporm.vue

--- app/javascript/packs/components/webs/Form.vue
+++ app/javascript/packs/components/webs/Form.vue
@@ -1,15 +1,71 @@
 <template>
     <div class="container">
         <h1>Invitation Wedding Form</h1>
+
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">氏名</span>
+            </div>
+            <input type="text" class="form-control" v-model="name"> 
+        </div>
+
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">出欠</span>
+            </div>
+            <input type="text" class="form-control" v-model="presence"> 
+        </div>
+        
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">住所</span>
+            </div>
+            <input type="text" class="form-control" v-model="address"> 
+        </div>
+
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">メールアドレス</span>
+            </div>
+            <input type="text" class="form-control" v-model="mail"> 
+        </div>
+
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">電話番号</span>
+            </div>
+            <input type="text" class="form-control" v-model="tel"> 
+        </div>
+
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">アレルギーなど</span>
+            </div>
+            <input type="text" class="form-control" v-model="allergy"> 
+        </div>
+
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">備考</span>
+            </div>
+            <input type="text" class="form-control" v-model="content"> 
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data: function() {
+        return {
+            name: "",
+            presence: "",
+            address: "",
+            mail: "",
+            tel: "",
+            allergy: "",
+            content: ""
+        }
+    }
 
 }
 </script>
-
-<style>
-
-</style>
