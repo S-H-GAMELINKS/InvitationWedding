1f136162956915bea8f6185e41247325722d24f4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-06 22:58:52 +0900

    init to using gmail

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,16 @@ module InvitationWedding
     # Using Slim
     config.generators.template_engine = :slim
 
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: ENV['G_MAIL'],
+      password: ENV['G_PASS'],
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
