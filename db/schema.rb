8770b6b07d3e90a7584ae7f17c87164f53c2c882
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 22:19:50 +0900

    Migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_092505) do
+ActiveRecord::Schema.define(version: 2019_02_08_131906) do
 
   create_table "attendances", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_01_22_092505) do
     t.text "other"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name_furi"
+    t.string "address"
+    t.string "yubin"
   end
 
 end
