3867fbe299fde69b11e001888c562c0207b51d5f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 21:59:16 +0900

    remove route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :attendances
   root 'web#index'
-  get "/about", to: "web#index"
-  get "/contact", to: "web#index"
   get "/form", to: "web#index"
 end
