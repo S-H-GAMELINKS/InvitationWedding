43ea39288ef59521ab7d25a3a30797a53a63a9ee
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 21:53:58 +0900

    remove

--- app/javascript/packs/components/layouts/Header.vue
+++ app/javascript/packs/components/layouts/Header.vue
@@ -2,17 +2,6 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <router-link class="navbar-brand" to="#">招待状アプリ</router-link>
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Menu
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <router-link to="/" class="dropdown-item">Top</router-link>
-                    <router-link to="/about" class="dropdown-item">About</router-link>
-                    <router-link to="/contact" class="dropdown-item">Contact</router-link>
-                    <router-link to="/form" class="dropdown-item">Form</router-link>
-                </div>
-            </div>
         </nav>
     </div>    
 </template>
