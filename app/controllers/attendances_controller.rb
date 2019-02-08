1da0323daaa3f94fc98b243f3627462e60ca1f92
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 22:21:32 +0900

    using :yubin

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -70,6 +70,6 @@ class AttendancesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def attendance_params
-      params.require(:attendance).permit(:name, :name_furi, :attend, :address, :email, :tel, :other)
+      params.require(:attendance).permit(:name, :name_furi, :attend, :yubin, :address, :email, :tel, :other)
     end
 end
