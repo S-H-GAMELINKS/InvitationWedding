a1f0d21c2438e6d50fec84b1ec3bd2677ace80b1
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 21:56:03 +0900

    fix link

--- app/javascript/packs/components/layouts/Header.vue
+++ app/javascript/packs/components/layouts/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <router-link class="navbar-brand" to="#">招待状アプリ</router-link>
+            <router-link class="navbar-brand" to="/">招待状アプリ</router-link>
         </nav>
     </div>    
 </template>
